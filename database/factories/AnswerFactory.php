91988c6ef0987c7f2f5bc178df628d681d5a6320
Cheikh Ahmed <cheikhrim8@gmail.com>
2020-07-18 09:30:06 +0000

    creating factories and seeding the database

--- database/factories/AnswerFactory.php
+++ database/factories/AnswerFactory.php
@@ -3,10 +3,18 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\Answer;
+use App\Model\Question;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Answer::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'user_id' => function () {
+            return User::all()->random();
+        },
+        'question_id' => function () {
+            return Question::all()->random();
+        }
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Model\Answer;
+use App\Model\Category;
+use App\Model\Like;
+use App\Model\Question;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +17,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(User::class , 10)->create();
+        factory(Category::class ,5)->create();
+        factory(Question::class ,10)->create();
+        factory(Answer::class , 50)->create()->each(function ($answer){
+            return $answer->likes()->save(factory(Like::class)->make());
+        });
     }
 }
